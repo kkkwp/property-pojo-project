d297b81c93669f76f0b54d00d11bc0dda65e57e2
kkkwp <chxrryda@gmail.com>
2025-08-21 13:32:00 +0900

    feat: enum 정의

---
+++ src/domain/enums/ContractStatus.java
@@ -0,0 +1,5 @@
+package domain.enums;
+
+public enum ContractStatus {
+	PENDING, COMPLETED, CANCELLED
+}
---
+++ src/domain/enums/PropertyStatus.java
@@ -0,0 +1,5 @@
+package domain.enums;
+
+public enum PropertyStatus {
+	AVAILABLE, IN_CONTRACT, COMPLETED
+}
